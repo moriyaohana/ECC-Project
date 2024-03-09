694199e28f87bf264f43148ee202708379101028
moriya ohana <moriyaohana@gmail.com>
2024-03-09 18:31:36 +0200

    refactored AudioTrack construction

--- TextOverAudioApp/app/src/main/java/com/sipl/textoveraudioapp/FirstFragment.kt
+++ TextOverAudioApp/app/src/main/java/com/sipl/textoveraudioapp/FirstFragment.kt
@@ -1,5 +1,6 @@
 package com.sipl.textoveraudioapp
 
+import android.media.AudioAttributes
 import android.media.AudioFormat
 import android.media.AudioManager
 import android.media.AudioTrack
@@ -28,7 +29,7 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
@@ -38,35 +39,19 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val python: Python = Python.getInstance();
+        val python: Python = Python.getInstance()
 
-        val textOverSoundModule: PyObject = python.getModule("text_over_sound");
-        val textOverSound: PyObject = textOverSoundModule.callAttr("TextOverSound",250, 10, 20, 16, 3, 16);
+        //initialization
+        val textOverSoundModule: PyObject = python.getModule("text_over_sound")
+        val textOverSound: PyObject = textOverSoundModule.callAttr("TextOverSound",250, 10, 20, 16, 3, 16)
 
+        //event: the user sending a message
         binding.sendMessage.setOnClickListener {
-            val inputText: String = binding.messageInput.text.toString();
-
-            val encodedMessage = textOverSound.callAttr("string_to_pcm_data", inputText).toJava(ByteArray::class.java);
-
-            val buffSize = AudioTrack.getMinBufferSize(
-                16000,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT
-            )
-
-            val audio = AudioTrack(
-                AudioManager.STREAM_MUSIC,
-                16000,  //sample rate
-                AudioFormat.CHANNEL_OUT_MONO,  //2 channel
-                AudioFormat.ENCODING_PCM_16BIT,  // 16-bit
-                buffSize,
-                AudioTrack.MODE_STREAM
-            )
-
-            audio.play();
-            audio.write(encodedMessage, 0, encodedMessage.size);
-            audio.stop();
+            val inputText: String = binding.messageInput.text.toString()
+            messageToSound(textOverSound, inputText, 16000)
         }
+
+
 
 
         binding.buttonFirst.setOnClickListener {
@@ -77,5 +62,34 @@ class FirstFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun messageToSound(textOverSound: PyObject, inputText: String, sampleRateHz: Int ){
+        val encodedMessage = textOverSound.callAttr("string_to_pcm_data", inputText).toJava(ByteArray::class.java)
+
+        val buffSize = AudioTrack.getMinBufferSize(
+            sampleRateHz,
+            AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT
+        )
+
+        val audio = AudioTrack(
+            AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build(),
+            AudioFormat.Builder()
+                .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
+                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                .setSampleRate(sampleRateHz)
+                .build(),
+            buffSize,
+            AudioTrack.MODE_STREAM,
+            AudioManager.AUDIO_SESSION_ID_GENERATE
+        )
+
+        audio.play()
+        audio.write(encodedMessage, 0, encodedMessage.size)
+        audio.stop()
     }
 }
